b3b63257a8606501307623ae391d7ce5d1aa8a37
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-07-16 16:37:29 +0530

    machine coding

---
+++ src/components/CountDownTimer.js
@@ -0,0 +1,37 @@
+import { useState, useEffect, useRef } from "react";
+
+export default function CountDownTimer({ initialCount }) {
+  const [currentCount, setCurrentCount] = useState(initialCount);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const timer = useRef(null);
+
+  useEffect(() => {
+    if (currentCount > 0 && !isPaused) {
+      timer.current = setInterval(() => {
+        setCurrentCount((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+    return () => clearInterval(timer.current);
+  }, [currentCount, isPaused]);
+
+  const onPause = () => {
+    setIsPaused(true);
+    clearInterval(timer.current);
+  };
+  const onResume = () => {
+    setIsPaused(false);
+  };
+  const onReset = () => {
+    setCurrentCount(10);
+  };
+
+  return (
+    <>
+      <h1>{currentCount}</h1>
+      <button onClick={onPause}>pause</button>
+      <button onClick={onResume}>resume</button>
+      <button onClick={onReset}>Reset</button>
+    </>
+  );
+}
